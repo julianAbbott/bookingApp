913ede8aea7a310db490f5f3d02a5bd5986fd753
Julian Abbott <julianabbott@Titus-Abbotts-Mac-mini.local>
2022-02-26 19:09:33 -0500

    trying a 3rd time lol

--- src/dateInput.js
+++ src/dateInput.js
@@ -3,13 +3,37 @@ import PropTypes from "prop-types";
 import ReactDOM from 'react-dom';
 import './dateInput.css';
 export default class DateInput extends React.Component {
-
-
+    // constructor(props){
+    //     super(props);
+    //     this.state({ date: ''});
+    // }   
+    constructor() {
+        super();
+        this.state = {
+            
+        };
+        this.onInputchange = this.onInputchange.bind(this);
+        this.onSubmitForm = this.onSubmitForm.bind(this);
+      }
+    
+      onInputchange(event) {
+        this.setState({
+          [event.target.name]: event.target.value
+        });
+      }
+    
+      onSubmitForm() {
+        console.log(this.state)
+      }
     
     render(){
+        const { items } = this.state;
         return(    
         <div className="component-display">
-            <input type="date" id="dt" onchange="mydate1();"/>
+            <input type="date" id="dt" onChange={this.onInputchange}/>
+            <div>
+                <button onClick={this.onSubmitForm}>Submit</button>
+            </div>
         </div>);
         
         // <body>
